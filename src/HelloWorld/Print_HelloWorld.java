fa41b42275d54491cb7968ff32f9f49e2ed63e59
rutwikgawande1919 <rutwikgawande123@gmail.com>
2026-05-29 10:26:27 +0530

    Hello World

---
+++ src/HelloWorld/Print_HelloWorld.java
@@ -0,0 +1,15 @@
+package HelloWorld;
+
+public class Print_HelloWorld {
+
+	public static void main(String[] args) {
+
+		//System.out.println("HElloWorld");
+		
+		 int a =13;
+		 int b =15;
+		 int c =a+b;
+		 System.out.println(c);
+	}
+
+}
