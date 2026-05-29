332accad35813c73dd2039c6ab62566b7f1f7c46
rutwikgawande1919 <rutwikgawande123@gmail.com>
2026-05-29 10:29:14 +0530

    If Else condition

---
+++ src/Conditions/Nested_IF_ELSE_Condition.java
@@ -0,0 +1,22 @@
+package Conditions;
+
+public class Nested_IF_ELSE_Condition {
+
+	public static void main(String[] args) {
+
+		int a =12;
+		
+		if(a % 2 ==0) {
+			System.out.println("It is a even number");
+			
+		if(a % 3 ==0) {
+			System.out.println("It is a good number");
+		}	
+		}
+		else {
+			System.out.println("It is a bad number");
+		}
+		
+	}
+
+}
