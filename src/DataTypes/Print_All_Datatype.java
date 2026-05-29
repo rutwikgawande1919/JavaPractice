85484efa0f2d9d0215503c35a56d538879b1d776
rutwikgawande1919 <rutwikgawande123@gmail.com>
2026-05-29 10:28:07 +0530

    All Datatypes

---
+++ src/DataTypes/Print_All_Datatype.java
@@ -0,0 +1,38 @@
+package DataTypes;
+
+public class Print_All_Datatype {
+
+	
+
+	public static void main(String[] args) {
+//primitive data types
+		int variable = 13;
+		System.out.println(variable);
+		
+		short id = 12345;
+		System.out.println("id is :"+id);
+		
+		long mobnum = 2354657688l;
+		System.out.println("mobile num is : "+mobnum);
+		
+		boolean truefalse = true;
+		System.out.println("Boolean value is :"+truefalse);
+		
+		byte gb = 64;
+		System.out.println("data in gb is  :"+gb);
+		
+		float percent = 93.34f;
+		System.out.println("class percentage is  :"+percent);
+		
+		double number = 56;
+		System.out.println("double number is  :"+number);
+		
+		
+//non primitive datatype
+		String a ="Rutwik";
+		System.out.println(a);
+		
+		
+	}
+
+}
