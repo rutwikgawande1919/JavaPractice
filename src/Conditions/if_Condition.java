6d46a23f2e55ea14d72cd6fe9fd00a288bb01d6d
rutwikgawande1919 <rutwikgawande123@gmail.com>
2026-05-29 10:28:51 +0530

    If Consition

---
+++ src/Conditions/if_Condition.java
@@ -0,0 +1,18 @@
+package Conditions;
+
+public class if_Condition {
+
+	public static void main(String[] args) {
+		
+		int a = 16;
+		
+		if(a % 2 == 0) {
+			System.out.println("It is a odd number");
+		}
+ 
+		else {
+			System.out.println("It is a even number");
+		}
+	}
+
+}
