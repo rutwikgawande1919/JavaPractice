c59e814c82758715dc4439801520b0ac2139ad8f
rutwikgawande1919 <rutwikgawande123@gmail.com>
2026-05-30 08:39:31 +0530

    Encapsulation1 is implemented

---
+++ src/Encapsulation1/Owner.java
@@ -0,0 +1,15 @@
+package Encapsulation1;
+
+public class Owner {
+ 
+	private String owner;
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+	
+}
