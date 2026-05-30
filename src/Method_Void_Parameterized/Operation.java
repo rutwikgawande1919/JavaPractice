92ee52ab768f0a40f561e430cc487c37dbbb3ece
rutwikgawande1919 <rutwikgawande123@gmail.com>
2026-05-30 23:33:14 +0530

    void parameterized method

---
+++ src/Method_Void_Parameterized/Operation.java
@@ -0,0 +1,25 @@
+package Method_Void_Parameterized;
+
+public class Operation {
+
+	public void Addition(int a, int b) {
+		int addition=a+b;
+		System.out.println("Addition is :"+addition);
+	}
+	
+	public void multiplication(int a,int b) {
+		int multiply=a*b;
+		System.out.println("Multiplication is :"+multiply);
+	}
+	
+	public void Division(int a,int b) {
+		int division=a/b;
+		System.out.println("Division is :"+division);
+	}
+	
+	public void mod(int a,int b) {
+		int mod=a%b;
+		System.out.println("Mod is :"+mod);
+	}
+	
+}
---
+++ src/Method_Void_Parameterized/Test.java
@@ -0,0 +1,15 @@
+package Method_Void_Parameterized;
+
+public class Test {
+public static void main(String[] args) {
+	Operation o = new Operation();
+	
+	o.Addition(15, 17);
+	
+	o.multiplication(18, 3);
+	
+	o.Division(10, 1);
+	
+	o.mod(10, 6);
+}
+}
