eda1e93b37f057fe9eb987b039afcdcde038460d
rutwikgawande1919 <rutwikgawande123@gmail.com>
2026-05-30 19:59:56 +0530

    polymorphism compile time

---
+++ src/Polymorphism_compiletime/Calculator.java
@@ -0,0 +1,26 @@
+package Polymorphism_compiletime;
+
+public class Calculator {
+
+//sum	
+	public void sum(int a,int b) {
+		System.out.println("sum :"+(a+b));
+	}
+	public void sum(int a,int b,int c) {
+		System.out.println("sum :"+(a+b+c));
+	}
+	public void sum(int a,int b,int c,int d) {
+		System.out.println("sum :"+(a+b+c+d));
+	}
+	
+//substraction
+	public void substract(int a,int b) {
+		System.out.println("substract :"+(a-b));
+	}
+	public void substract(int a,int b,int c) {
+		System.out.println("substract :"+(a-b-c));
+	}
+	public void substract(int a,int b, int c,int d) {
+		System.out.println("substract :"+(a-b-c-d));
+	}
+}
