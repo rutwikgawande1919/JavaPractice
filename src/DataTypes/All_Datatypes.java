5935b0492d5c7f49b30530fef66f1f30f1644a74
rutwikgawande1919 <rutwikgawande123@gmail.com>
2026-05-31 18:20:28 +0530

    Datatypes

---
+++ src/DataTypes/All_Datatypes.java
@@ -0,0 +1,32 @@
+package DataTypes;
+
+public class All_Datatypes {
+public static void main(String[] args) {
+	 int a=12;
+	 System.out.println("A is :"+a);
+	 
+	 short age = 32;
+	 System.out.println("Age is :"+age);
+	 
+	 long mono=123456789087l;
+	 System.out.println("Mobile no is :"+mono);
+	 
+	 byte GB=5;
+	 System.out.println("Data in GB is :"+GB);
+	 
+	 float value=57.50f;
+	 System.out.println("Float value is :"+value);
+	 
+	 double percentage=89.30;
+	 System.out.println("percentage is :"+percentage);
+	 
+	 boolean isjavafun=true;
+	 System.out.println("True or False :"+isjavafun);
+	 
+	 String name ="Rutwik";
+	 System.out.println("name is :"+name);
+	 
+	 
+}
+	
+}
