0849a4ac6c3dd23739c52824a833b365c3919b34
rutwikgawande1919 <rutwikgawande123@gmail.com>
2026-05-31 18:30:14 +0530

    String

---
+++ src/String/StringTest.java
@@ -0,0 +1,14 @@
+package String;
+
+public class StringTest {
+
+	public static void main(String[] args) {
+		String day ="Tuesday";
+		
+		//for(int i=0;i<=day.length()-1;i++) {
+		//	System.out.println(day.charAt(i));
+	//	}
+		int length =day.length();
+		System.out.println(length);
+	}
+}
