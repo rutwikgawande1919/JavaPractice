48e710f2620961401d7df245423665f21d46648f
rutwikgawande1919 <rutwikgawande123@gmail.com>
2026-06-01 15:08:14 +0530

    return type parameterized

---
+++ src/Method_ReturnType_parameterized/Shrikant2.java
@@ -0,0 +1,9 @@
+package Method_ReturnType_parameterized;
+
+public class Shrikant2 {
+
+	public String Details(String fname) {
+		
+		return fname;
+	}
+}
---
+++ src/Method_ReturnType_parameterized/Test2.java
@@ -0,0 +1,9 @@
+package Method_ReturnType_parameterized;
+
+public class Test2 {
+public static void main(String[] args) {
+	Shrikant2 s = new Shrikant2();
+	String name =s.Details("Shrikant patil");
+	System.out.println(name);
+}
+}
