f4dcac7afd53ade8e2740feb88f479d0ee6a8015
rutwikgawande1919 <rutwikgawande123@gmail.com>
2026-06-01 14:58:06 +0530

    if else condition implemented

---
+++ src/Conditions/ifelseCondition.java
@@ -0,0 +1,15 @@
+package Conditions;
+
+public class ifelseCondition {
+	public static void main(String[] args) {
+		
+		int a =12;
+		
+		if(a%2==0) {
+			System.out.println("it is even number");
+		}else {
+			System.out.println("it is odd number");
+		}
+	}
+
+}
