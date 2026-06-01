ce7e9afd61b41d6158e548a8369f766ceef0ac82
rutwikgawande1919 <rutwikgawande123@gmail.com>
2026-06-01 15:09:28 +0530

    void parameterized method implemented

---
+++ src/Method_Void_Parameterized/Test2.java
@@ -0,0 +1,8 @@
+package Method_Void_Parameterized;
+
+public class Test2 {
+public static void main(String[] args) {
+	sanket2 s = new sanket2();
+	s.Details("Sanket", 24);
+}
+}
---
+++ src/Method_Void_Parameterized/sanket2.java
@@ -0,0 +1,9 @@
+package Method_Void_Parameterized;
+
+public class sanket2 {
+
+	public void Details(String name,int age) {
+		System.out.println(name);
+		System.out.println(age);
+	}
+}
