d90d4daf85e280ec84c129bf11487f7425b8d1d4
rutwikgawande1919 <rutwikgawande123@gmail.com>
2026-06-01 15:11:23 +0530

    return method

---
+++ src/Methods_ReturnType/Operation1.java
@@ -0,0 +1,18 @@
+package Methods_ReturnType;
+
+public class Operation1 {
+
+	public int Division() {
+		int a=15;
+		int b=6;
+		int division=a/b;
+		return division;
+	}
+	
+	public int Mod() {
+		int a =15;
+		int b=6;
+		int mod =a%b;
+		return mod;
+	}
+}
---
+++ src/Methods_ReturnType/Test1.java
@@ -0,0 +1,13 @@
+package Methods_ReturnType;
+
+public class Test1 {
+public static void main(String[] args) {
+	Operation1 o=new Operation1();
+	int division=o.Division();
+	System.out.println(division);
+	
+	
+	int mod=o.Mod();
+	System.out.println(mod);
+}
+}
