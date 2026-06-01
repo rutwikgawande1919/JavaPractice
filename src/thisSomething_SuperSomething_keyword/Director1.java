7134f6518bc8d6f6eb35542e15680f8da29a3898
rutwikgawande1919 <rutwikgawande123@gmail.com>
2026-06-01 15:23:31 +0530

    this something super something implemented

---
+++ src/thisSomething_SuperSomething_keyword/Director1.java
@@ -0,0 +1,11 @@
+package thisSomething_SuperSomething_keyword;
+
+public class Director1 {
+
+	public int age=45;
+	
+	public void getage() {
+		System.out.println("Director age :50");
+	}
+	
+}
---
+++ src/thisSomething_SuperSomething_keyword/Test1.java
@@ -0,0 +1,8 @@
+package thisSomething_SuperSomething_keyword;
+
+public class Test1 {
+public static void main(String[] args) {
+	Employee1 e=new Employee1();
+	e.something();
+}
+}
