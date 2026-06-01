f7688611230f9f3bf4e2cff3bcaed26b4c813b70
rutwikgawande1919 <rutwikgawande123@gmail.com>
2026-06-01 14:55:58 +0530

    Casting implemented

---
+++ src/Casting/Casting.java
@@ -0,0 +1,29 @@
+package Casting;
+
+public class Casting {
+	public static void main(String[] args) {
+		
+		int a=12;
+		double b =a;
+		System.out.println(b);
+		
+		short age= 23;
+		long c=age;
+		System.out.println(c);
+		
+		double f=89.44;
+		byte s =(byte) f;
+		System.out.println(s);
+		
+		double n=44.55;
+		int e=(int) n;
+		System.out.println(e);
+		
+		long num =393745658328l;
+		short m=(short) num;
+		System.out.println(m);
+		
+		
+	}
+
+}
