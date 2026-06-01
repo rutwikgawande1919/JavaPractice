3384d4ed8df1f208fcfddd6f1da80334da82baf8
rutwikgawande1919 <rutwikgawande123@gmail.com>
2026-06-01 15:10:32 +0530

    methods implemented

---
+++ src/Methods/Rahul1.java
@@ -0,0 +1,14 @@
+package Methods;
+
+public class Rahul1 {
+
+	public void information() {
+		
+		 int adharno =54564366;
+		 String panno="dkbfg4827g";
+		 int licno=9737646;
+		 System.out.println("adhar no is :"+adharno);
+		 System.out.println("Pan no is :"+panno);
+		 System.out.println("Licence no is :"+licno);
+	}
+}
---
+++ src/Methods/Test1.java
@@ -0,0 +1,8 @@
+package Methods;
+
+public class Test1 {
+public static void main(String[] args) {
+	Rahul1 r= new Rahul1();
+	r.information();
+}
+}
