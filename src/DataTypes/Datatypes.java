1d109d3414b1faa51b9641e79091f6aac86b887c
rutwikgawande1919 <rutwikgawande123@gmail.com>
2026-06-03 00:31:48 +0530

    datatypes implemented

---
+++ src/DataTypes/Datatypes.java
@@ -0,0 +1,33 @@
+package DataTypes;
+
+public class Datatypes {
+public static void main(String[] args) {
+	
+	int a =12;
+	System.out.println(a);
+	
+	short age =45;
+	System.out.println(age);
+	
+	long phonenum=73543823633453l;
+	System.out.println(phonenum);
+	
+	byte gb=5;
+	System.out.println(gb);
+	
+	double percent =55.77;
+	System.out.println(percent);
+	
+	float per = 88.99f;
+	System.out.println(per);
+	
+	char ch='H';
+	System.out.println(ch);
+	
+	boolean isjavafun=true;
+	System.out.println(isjavafun);
+	
+	String name ="Rutwik";
+	System.out.println(name);
+}
+}
