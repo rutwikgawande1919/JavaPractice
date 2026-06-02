af2f095ddf78733f2b63654391eb8d32c09320e6
rutwikgawande1919 <rutwikgawande123@gmail.com>
2026-06-03 00:30:07 +0530

    casting1 implemented

---
+++ src/Casting/casting1.java
@@ -0,0 +1,21 @@
+package Casting;
+
+public class casting1 {
+public static void main(String[] args) {
+	int a =12;
+	double b=a;
+	System.out.println(b);
+	
+	float c=55.77f;
+	double d=c;
+	System.out.println(d);
+	
+	long n=9857743328l;
+	short f=(short) n;
+	System.out.println(f);
+	
+	double j=88.90;
+	int r=(int) j;
+	System.out.println(r);
+}
+}
