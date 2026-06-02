7f5ea5eb78de463a2ce119294017fd05d7e6c236
rutwikgawande1919 <rutwikgawande123@gmail.com>
2026-06-03 00:39:22 +0530

    void methods implemented

---
+++ src/Methods/Test2.java
@@ -0,0 +1,9 @@
+package Methods;
+
+public class Test2 {
+public static void main(String[] args) {
+	operation2 op=new operation2();
+	
+	op.getname();
+}
+}
---
+++ src/Methods/operation2.java
@@ -0,0 +1,8 @@
+package Methods;
+
+public class operation2 {
+
+	public void getname() {
+		System.out.println("Rutwik");
+	}
+}
