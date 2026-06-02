c12fb6259a7540324d5339ebfa0013ae1bbfc0f5
rutwikgawande1919 <rutwikgawande123@gmail.com>
2026-06-03 00:38:34 +0530

    parameterized void method implemented

---
+++ src/Method_Void_Parameterized/Test3.java
@@ -0,0 +1,8 @@
+package Method_Void_Parameterized;
+
+public class Test3 {
+public static void main(String[] args) {
+	operation3 op = new operation3();
+	op.multiplication(15, 17);
+}
+}
---
+++ src/Method_Void_Parameterized/operation3.java
@@ -0,0 +1,9 @@
+package Method_Void_Parameterized;
+
+public class operation3 {
+
+	public void multiplication(int a,int b) {
+		int mul=a*b;
+		System.out.println(mul);
+	}
+}
