14a19a5f81b7d8db3d647ca3ec7bf9b11b56110f
rutwikgawande1919 <rutwikgawande123@gmail.com>
2026-06-04 00:05:07 +0530

    void methods implemented

---
+++ src/Methods/Test3.java
@@ -0,0 +1,9 @@
+package Methods;
+
+public class Test3 {
+public static void main(String[] args) {
+	information3 i=new  information3();
+	
+	i.Details();
+}
+}
---
+++ src/Methods/information3.java
@@ -0,0 +1,15 @@
+package Methods;
+
+public class information3 {
+
+	public void Details(){
+		
+		String name="Rutwik";
+		String Scname="Swamivivekanand";
+		String CCname="Bhausaheb pottey";
+		
+		System.out.println(name);
+		System.out.println(Scname);
+		System.out.println(CCname);
+	}
+}
