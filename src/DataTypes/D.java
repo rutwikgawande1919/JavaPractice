8939572902180f8636670aaa5fccf6ab75774990
rutwikgawande1919 <rutwikgawande123@gmail.com>
2026-06-03 23:54:16 +0530

    Datatypes implemented

---
+++ src/DataTypes/D.java
@@ -0,0 +1,30 @@
+package DataTypes;
+
+public class D {
+public static void main(String[] args) {
+	
+	int a =12;
+	System.out.println(a);
+	
+	short age =56;
+	System.out.println(age);
+	
+	long num=83463567290l;
+	System.out.println(num);
+	
+	byte gb=5;
+	System.out.println(gb);
+	
+	float percent=66.4f;
+	System.out.println(percent);
+	
+	double percent1=77.33;
+	System.out.println(percent1);
+	
+	char c='H';
+	System.out.println(c);
+	
+	boolean javaisfun=true;
+	System.out.println(javaisfun);
+}
+}
