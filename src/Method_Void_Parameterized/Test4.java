710e67026871a60c731e0929a6944469b79c1d87
rutwikgawande1919 <rutwikgawande123@gmail.com>
2026-06-04 00:04:23 +0530

    void parameterized method

---
+++ src/Method_Void_Parameterized/Test4.java
@@ -0,0 +1,8 @@
+package Method_Void_Parameterized;
+
+public class Test4 {
+public static void main(String[] args) {
+	operation4 op = new operation4();
+	op.Addition(17, 19);
+}
+}
---
+++ src/Method_Void_Parameterized/operation4.java
@@ -0,0 +1,9 @@
+package Method_Void_Parameterized;
+
+public class operation4 {
+
+	public void Addition(int a, int b) {
+		int c=a+b;
+		System.out.println(c);
+	}
+}
