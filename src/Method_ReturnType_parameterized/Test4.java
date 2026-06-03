780d7410e24bc74f5a61707083480c239168e8d9
rutwikgawande1919 <rutwikgawande123@gmail.com>
2026-06-04 00:03:44 +0530

    return type parameterized method implemented

---
+++ src/Method_ReturnType_parameterized/Test4.java
@@ -0,0 +1,9 @@
+package Method_ReturnType_parameterized;
+
+public class Test4 {
+public static void main(String[] args) {
+	operation4 op=new operation4();
+	int mul=op.Multiplication(16, 30);
+	System.out.println(mul);
+}
+}
---
+++ src/Method_ReturnType_parameterized/operation4.java
@@ -0,0 +1,9 @@
+package Method_ReturnType_parameterized;
+
+public class operation4 {
+
+	public int Multiplication(int a,int b) {
+		int multi=a*b;
+		return multi;
+	}
+}
