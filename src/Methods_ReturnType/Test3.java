1abfd56803d3e852f9f952da2e0df3383a541faf
rutwikgawande1919 <rutwikgawande123@gmail.com>
2026-06-04 00:05:46 +0530

    return type method implemented

---
+++ src/Methods_ReturnType/Test3.java
@@ -0,0 +1,10 @@
+package Methods_ReturnType;
+
+public class Test3 {
+public static void main(String[] args) {
+	operation3 op=new operation3();
+	
+	int sum =op.Addition();
+	System.out.println(sum);
+}
+}
---
+++ src/Methods_ReturnType/operation3.java
@@ -0,0 +1,11 @@
+package Methods_ReturnType;
+
+public class operation3 {
+
+	public int Addition() {
+		int a =12;
+		int b =14;
+		int c=a+b;
+		return c;
+	}
+}
