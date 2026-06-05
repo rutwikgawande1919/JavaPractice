99a26768e3b5baa8fb0862417b83bb9b3a578650
rutwikgawande1919 <rutwikgawande123@gmail.com>
2026-06-05 14:02:29 +0530

    void method implemented

---
+++ src/Methods/Details4.java
@@ -0,0 +1,8 @@
+package Methods;
+
+public class Details4 {
+public static void main(String[] args) {
+	Laptop4 l=new Laptop4();
+	l.lapdetails();
+}
+}
---
+++ src/Methods/Laptop4.java
@@ -0,0 +1,16 @@
+package Methods;
+
+public class Laptop4 {
+
+	
+	public void lapdetails() {
+		
+		long launchyear=2026;
+		String laptopcompany="Asus";
+		String lapmodel="vivobook 15 AMD ryzen 5";
+		
+		System.out.println("laptop launch year :"+launchyear);
+		System.out.println("laptop company :"+laptopcompany);
+		System.out.println("laptop model :"+lapmodel);
+	}
+}
