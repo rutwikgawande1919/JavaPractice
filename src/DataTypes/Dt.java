f66b218e9e1276f4c641923ef104de9b2e29ec03
rutwikgawande1919 <rutwikgawande123@gmail.com>
2026-06-05 13:37:39 +0530

    datatypes implemented

---
+++ src/DataTypes/Dt.java
@@ -0,0 +1,32 @@
+package DataTypes;
+
+public class Dt {
+public static void main(String[] args) {
+	 int a =12;
+	 System.out.println(a);
+	 
+	 short age=56;
+	 System.out.println(age);
+	 
+	 long pnum= 73467567558656l;
+	 System.out.println(pnum);
+	 
+	 byte gb=5;
+	 System.out.println(gb);
+	 
+	 float per=55.4f;
+	 System.out.println(per);
+
+	 double percent=88.77;
+	 System.out.println(percent);
+	 
+	 char ch='R';
+	 System.out.println(ch);
+	 
+	 boolean javafun=true;
+	 System.out.println(javafun);
+	 
+	 String name ="Rutwik";
+	 System.out.println(name);
+}
+}
