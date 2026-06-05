1c1dd39f93f537686168ab55e9c800b625ff82e9
rutwikgawande1919 <rutwikgawande123@gmail.com>
2026-06-05 14:03:25 +0530

    returntype method implemented

---
+++ src/Methods_ReturnType/Details4.java
@@ -0,0 +1,10 @@
+package Methods_ReturnType;
+
+public class Details4 {
+public static void main(String[] args) {
+	Laptop4 lp =new Laptop4();
+	String details =lp.lapdetails();
+	System.out.println(details);
+	
+}
+}
---
+++ src/Methods_ReturnType/Laptop4.java
@@ -0,0 +1,14 @@
+package Methods_ReturnType;
+
+public class Laptop4 {
+
+	public String lapdetails() {
+		
+		String company="Asus->";
+		String lapname=" Asus vivibook->";
+		String lapmodel=" AMD ryzen 5";
+		
+		String details=company +lapname +lapmodel;
+		return details;
+	}
+}
