c215aa6e0b18512dc8e0f8401a9e93e88cecf3ff
rutwikgawande1919 <rutwikgawande123@gmail.com>
2026-06-05 14:01:03 +0530

    void parameterized method implemented

---
+++ src/Method_Void_Parameterized/Details5.java
@@ -0,0 +1,10 @@
+package Method_Void_Parameterized;
+
+public class Details5 {
+public static void main(String[] args) {
+	Laptop5 lp = new Laptop5();
+	
+	lp.lapdetails("Asus","vivobook 15", "Amd ryzen 5");
+	
+}
+}
---
+++ src/Method_Void_Parameterized/Laptop5.java
@@ -0,0 +1,10 @@
+package Method_Void_Parameterized;
+
+public class Laptop5 {
+
+	public void lapdetails(String company,String lapname,String lapmodel ) {
+		
+	String details = company+lapname+lapmodel;
+	System.out.println(details);
+	}
+}
